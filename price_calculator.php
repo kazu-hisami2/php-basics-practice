645537e18644ccc245cca45ec89d7e9ef1297148
Kazuki Hisamitsu <hisamitsu0619.0619@gmail.com>
2026-06-30 22:22:39 +0900

    add tax-percentage veriable
    コードの見やすさのため消費税(%)をtax_per変数として定義

--- price_calculator.php
+++ price_calculator.php
@@ -1,17 +1,20 @@
 <?php
-date_default_timezone_set('Asia/Tokyo'); //タイムゾーンを東京に指定
-// 7-1-6_hands-on/practice/price_calculator.php
+//名称・単価・個数・税率
 $product_name = "ノートパソコン";
 $price = 80000;
 $quantity = 2;
 $tax_rate = 0.1;
+$tax_per = $tax_rate*100; //消費税率を百分率で表示
+
+//小計と税込み価格の計算
 $subtotal = $price*$quantity;
 $tax_amount = $subtotal*$tax_rate;
 $total = $subtotal+$tax_amount;
 
+//ブラウザ上で表示
 echo "商品名: {$product_name}<br>";
 echo "単価: ".number_format($price)."円<br>";
 echo "数量: ".number_format($quantity)."個<br>";
 echo "小計: ".number_format($subtotal)."円<br>";
-echo "消費税(".($tax_rate*100)."%): ".number_format($tax_amount)."円<br>";
+echo "消費税({$tax_per}%): ".number_format($tax_amount)."円<br>";
 echo "<b>合計金額: ".number_format($total)."円</b>";
